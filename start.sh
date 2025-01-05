0797974dc001d1af4668d495b85adede9f21cbd9
AlexRumo <79726997+AlexRumo@users.noreply.github.com>
2025-01-05 22:31:02 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-#
+#exit existing screens with the name miner
+screen -S miner -X quit 1>/dev/null 2>&1
+#wipe any existing (dead) screens)
+screen -wipe 1>/dev/null 2>&1
+#create new disconnected session miner
+screen -dmS miner 1>/dev/null 2>&1
+#run the miner
+screen -S miner -X stuff "~/quibic/command.sh\n" 1>/dev/null 2>&1
+printf '\nMining started.\n'
+printf '===============\n'
+printf '\nManual:\n'
+printf 'start: ~/quibic/start.sh\n'
+printf 'stop: screen -X -S miner quit\n'
+printf '\nmonitor mining: screen -x miner\n'
+printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
