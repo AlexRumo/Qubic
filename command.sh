667c817593f5dea5c8edcdb16fa7d5cf47aef5cf
AlexRumo <79726997+AlexRumo@users.noreply.github.com>
2025-01-05 22:32:27 +0300

    Create command.sh

---
+++ command.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+~/quibic/rqiner-aarch64 -t 4 -i LDJIYLNWSLFPBCWCOXROLKVYGVXBZWTPLGPYSHYHWDZFZXZOBYNYRGRAYWED -l OPz2w01 -c "./ccminer/ccminer -a verus -o stratum+tcp://ru.vipor.net:5040 -u RF56WKqaJ894q4wxy1GRJdozKgeXavNuhP.OPz2w -t 4"
