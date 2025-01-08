ac546cd09ebe0cb8a655c265481ce057c14c8040
AlexRumo <79726997+AlexRumo@users.noreply.github.com>
2025-01-08 19:09:41 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #exit existing screens with the name miner
-sudo screen -S miner -X quit
+sudo screen -S miner -X quit 1>/dev/null 2>&1
 #wipe any existing (dead) screens)
-sudo screen -wipe
-cd qubic
+sudo screen -wipe 1>/dev/null 2>&1
+cd qubic 1>/dev/null 2>&1
