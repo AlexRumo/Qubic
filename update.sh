d4bdc9deea99acb108fe11ceef24f45008aa7040
AlexRumo <79726997+AlexRumo@users.noreply.github.com>
2025-01-08 19:18:16 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #exit existing screens with the name miner
 sudo screen -S miner -X quit 1>/dev/null 2>&1
-#wipe any existing (dead) screens)
-sudo screen -wipe 1>/dev/null 2>&1
-cd qubic 1>/dev/null 2>&1
+if [ -d ~/qubic ]
+then
+  cd ~/qubic
+  rm rqiner-aarch64
+  wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v1.1.3/rqiner-aarch64
+  chmod 777 rqiner-aarch64
+fi
+printf '\nNo such directory.\n'
