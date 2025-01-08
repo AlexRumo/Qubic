782e7127e3e09e0e4d54605d9a1d73fdc030b5ed
AlexRumo <79726997+AlexRumo@users.noreply.github.com>
2025-01-08 19:07:22 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,9 +3,4 @@
 sudo screen -S miner -X quit
 #wipe any existing (dead) screens)
 sudo screen -wipe
-#
-cd quibic
-rm rqiner-aarch64
-wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v1.1.3c/rqiner-aarch64
-chmod 777 rqiner-aarch64
-reboot
+cd qubic
